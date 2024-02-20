9ed08c4963faa0b16af92d7beb1a085fa95041d8
JSH <skyjs135@gmail.com>
2024-02-20 13:28:29 +0900

    Initial commit

---
+++ src/test/java/com/js/withyou/WithyouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.js.withyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithyouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
