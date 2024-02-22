5a6200259d1db9b22d4fbb3e58fec3aa4983a79b
JSH <skyjs135@gmail.com>
2024-02-22 22:57:52 +0900

    subRegion 저장을 위한 dto 작성

---
+++ src/main/java/com/js/withyou/data/dto/Region/SubRegionSaveDto.java
@@ -0,0 +1,16 @@
+package com.js.withyou.data.dto.Region;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SubRegionSaveDto {
+
+    @NotBlank(message = "세부지역 이름 정보 없음")
+    private String subRegionName; //
+    @NotBlank(message = "지역 이름 정보 없음")
+    private Long regionId;
+
+}
