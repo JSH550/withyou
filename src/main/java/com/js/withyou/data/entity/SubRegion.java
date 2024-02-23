5db9e19a2693b1c4efda5cae15bce093d12e2d33
JSH <skyjs135@gmail.com>
2024-02-23 23:48:03 +0900

    subRegion과 연관관계 주입

--- src/main/java/com/js/withyou/data/entity/SubRegion.java
+++ src/main/java/com/js/withyou/data/entity/SubRegion.java
@@ -5,6 +5,9 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Getter
 @RequiredArgsConstructor
@@ -14,13 +17,15 @@ public class SubRegion {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long subRegionId ;
 
-    @Column(unique = true)//지역이름은 중복되면 안됨
+    @Column//광주시같이 중복될 가능성 있음, 중복허용
     private String subRegionName; //
 
     @ManyToOne
     @JoinColumn(name = "region_id",nullable = false)//상위카테고리는 null이면 안된다.
     private Region region;
 
+    @OneToMany(mappedBy = "subRegion", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Place> places = new ArrayList<>();
     public static SubRegion createSubRegion(String subRegionName, Region region) {
         SubRegion subRegion = new SubRegion();
         subRegion.subRegionName = subRegionName;
