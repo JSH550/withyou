e0d51e74b7e0b0a1cb13eeb9aee17e1e1558f36e
JSH <skyjs135@gmail.com>
2024-02-26 23:51:47 +0900

    categoryName로 조회하는 메서드 추가

--- src/main/java/com/js/withyou/repository/CategoryRepository.java
+++ src/main/java/com/js/withyou/repository/CategoryRepository.java
@@ -3,6 +3,9 @@ package com.js.withyou.repository;
 import com.js.withyou.data.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category,Long> {
+    Optional<Category> findByCategoryName(String name);
 
 }
