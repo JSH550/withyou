153068c3bd4cb7dcd7054e001a6c3bfccf59ab2d
JSH <skyjs135@gmail.com>
2024-02-27 23:57:21 +0900

    시도 정보와 시군구 정보로 시군구 entity 검색하는 메서드 추가(시군구 중복명 존재, 시도정보 추가로 해결)

--- src/main/java/com/js/withyou/service/SubRegionService.java
+++ src/main/java/com/js/withyou/service/SubRegionService.java
@@ -1,6 +1,7 @@
 package com.js.withyou.service;
 
 import com.js.withyou.data.dto.MemberDto;
+import com.js.withyou.data.dto.Region.RegionDto;
 import com.js.withyou.data.dto.Region.SubRegionCreateDto;
 import com.js.withyou.data.dto.Region.SubRegionDto;
 import com.js.withyou.data.entity.SubRegion;
@@ -8,10 +9,14 @@ import com.js.withyou.data.entity.SubRegion;
 import java.util.Optional;
 
 public interface SubRegionService {
-    SubRegionCreateDto  createSubRegion(SubRegionCreateDto subRegionCreateDto);
-    SubRegionCreateDto  updateSubRegion(SubRegionDto subRegionDto);
-//    SubRegionCreateDto  deleteSubRegion(Long subRegionId);
-    SubRegionCreateDto  getAllSubRegion(SubRegionCreateDto subRegionCreateDto);
-    SubRegionCreateDto  getSubRegionById(Long subRegionId);
+//    SubRegionCreateDto  createSubRegion(SubRegionCreateDto subRegionCreateDto);
+//    SubRegionCreateDto  updateSubRegion(SubRegionDto subRegionDto);
+////    SubRegionCreateDto  deleteSubRegion(Long subRegionId);
+//    SubRegionCreateDto  getAllSubRegion(SubRegionCreateDto subRegionCreateDto);
+//    SubRegionCreateDto  getSubRegionById(Long subRegionId);
+
+
+//    SubRegionDto getSubRegionByRegionNameAndSubRegionName(String regionName, String subRegionName);
+    SubRegion getSubRegionByRegionNameAndSubRegionName(String regionName, String subRegionName);
 
 }
