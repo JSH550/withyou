2d3d94c10f279fe8439499e7aee3585c78639cd3
JSH <skyjs135@gmail.com>
2024-03-01 23:58:51 +0900

    subregion 저장용 list 추가

--- src/main/java/com/js/withyou/data/dto/Region/RegionDto.java
+++ src/main/java/com/js/withyou/data/dto/Region/RegionDto.java
@@ -1,13 +1,18 @@
 package com.js.withyou.data.dto.Region;
 
+import com.js.withyou.data.entity.SubRegion;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
 
 @Setter
 @Getter
 @RequiredArgsConstructor
+@ToString
 public class RegionDto {
 
     @NotBlank
@@ -18,5 +23,8 @@ public class RegionDto {
     private String regionName;
 
     private String regionShortName; // 시도명의 줄임말 필드,ex) 충남 충북
+
+
+    private List<SubRegion> subRegions;
 
 }
