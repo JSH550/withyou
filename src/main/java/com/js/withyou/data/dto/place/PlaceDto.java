c718097365f46a1002f409c23e73bba807860fda
JSH <skyjs135@gmail.com>
2024-03-01 23:56:10 +0900

    HashSet을 통해 중복 체크를 위한 equals, hashCode 메소드 오버라이드

--- src/main/java/com/js/withyou/data/dto/place/PlaceDto.java
+++ src/main/java/com/js/withyou/data/dto/place/PlaceDto.java
@@ -4,9 +4,13 @@ import com.js.withyou.data.entity.Place;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Objects;
 
 @Getter
 @Setter
+@ToString
 public class PlaceDto {
 
     @NotBlank
@@ -24,11 +28,26 @@ public class PlaceDto {
 
     private String subRegionName; // SubRegion 엔티티의 이름만 전달
 
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;//비교하려는 두객체가 동등하면 true 반환
+        if (o == null || getClass() != o.getClass()) return false;//두객체가 같은 class인지 확인
+        PlaceDto placeDto= (PlaceDto) o;//타입 캐스팅
+        return placeId == placeDto.placeId;//필드값 동일 유무 비교
+
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(placeId);//placeId기준으로 해시 코드 반환
+    }
+
     public PlaceDto(){
 
     }
 
-    public PlaceDto setPlaceDto (Place place){
+    public PlaceDto convertToPlaceDto(Place place){
         PlaceDto placeDto = new PlaceDto();
         placeDto.setPlaceId(place.getPlaceId());
         placeDto.setPlaceName(place.getPlaceName());
