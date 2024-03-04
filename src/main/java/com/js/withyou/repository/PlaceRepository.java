b84043aa81a6e4a97d93f1a1ce3080f4af5c3eb4
JSH <skyjs135@gmail.com>
2024-03-04 23:39:01 +0900

    특정 키워드를 포함하는 장소명 조회 메서드 FETCH JOIN 으로 지연로딩 문제 최적화

--- src/main/java/com/js/withyou/repository/PlaceRepository.java
+++ src/main/java/com/js/withyou/repository/PlaceRepository.java
@@ -1,12 +1,28 @@
 package com.js.withyou.repository;
 
 import com.js.withyou.data.entity.Place;
+import com.js.withyou.data.entity.SubRegion;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
-public interface PlaceRepository extends JpaRepository<Place,Long> {
-List<Place> findByPlaceNameContaining(String keyword);
+public interface PlaceRepository extends JpaRepository<Place, Long> {
+    /**
+     * 장소명에 특정 키워드를 포함하는 장소를 조회합니다.
+     * 이 메서드는 JOIN FETCH로, 관련된 SubRegion과 Category를 함께 즉시 로딩합니다.
+     *
+     * @param keyword 조회할 장소명에 포함된 키워드
+     * @return 장소명에 특정 키워드를 포함하는 장소의 목록
+     */
+    @Query("SELECT p FROM Place p " +
+            "JOIN FETCH p.subRegion " +
+            "JOIN FETCH p.category "+
+            "WHERE p.placeName LIKE %:keyword%")
+    List<Place> findByPlaceNameContaining(String keyword);
+
+    @Query("SELECT p FROM Place p Where p.subRegion.subRegionId = :subregionId")
+    List<Place> findBySubregionId(Long subregionId);
 
 
 }
