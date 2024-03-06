d68faa3c2189f093ef965c0d37e36893def07317
JSH <skyjs135@gmail.com>
2024-03-06 23:57:38 +0900

    키워드로 시군구(subRegion) 검색하는 메서드 추가

--- src/main/java/com/js/withyou/repository/SubRegionRepository.java
+++ src/main/java/com/js/withyou/repository/SubRegionRepository.java
@@ -1,13 +1,29 @@
 package com.js.withyou.repository;
 
+import com.js.withyou.data.dto.Region.SubRegionDto;
 import com.js.withyou.data.entity.SubRegion;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SubRegionRepository extends JpaRepository<SubRegion,Long> {
 
     Optional<SubRegion> findBySubRegionName(String subRegionName);
+
+    /**
+     * 주어진 키워드를 포함하는 하위 지역(SubRegion)을 데이터베이스에서 검색합니다.
+     * 검색된 하위 지역은 지역(Region) 엔티티와의 조인(fetch join)을 통해 함께 로드됩니다.
+     * @param keyword 검색에 사용할 키워드
+     * @return 키워드를 포함하는 하위 지역(SubRegion)의 목록을 반환합니다.
+     */
+    @Query("SELECT s from SubRegion s " +
+            "JOIN FETCH s.region " +
+            "WHERE s.subRegionName LIKE %:keyword%")
+    List<SubRegion> findBySubRegionNameContaining(String keyword) ;
+//    Optional<SubRegion> findByRegionIdAndSubRegionName(Long regionId,String subRegionName);
+//    Optional<SubRegion> findByRegion_RegionIdAndSubRegionName(Long regionId, String subRegionName);
 
 
 
