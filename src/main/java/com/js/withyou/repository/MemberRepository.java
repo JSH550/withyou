9c481a14706c3b08cac1bba3303de5c5f8e07f00
JSH <skyjs135@gmail.com>
2024-03-07 23:54:37 +0900

    회원(Member) repository 추가

---
+++ src/main/java/com/js/withyou/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.js.withyou.repository;
+
+import com.js.withyou.data.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member,Long> {
+    Optional<Member> findByMemberName(String memberName);
+    Optional<Member> findByMemberEmail(String memberEmail);
+}
