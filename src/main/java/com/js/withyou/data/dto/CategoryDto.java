54db4ef6e75158de587a642b8181c04a81b81d7e
JSH <skyjs135@gmail.com>
2024-03-20 23:16:07 +0900

    feat: 시설(place)정보를 저장하는 객체를 추가하였습니다     * PlaceDto로 변환하여 받습니다.

--- src/main/java/com/js/withyou/data/dto/CategoryDto.java
+++ src/main/java/com/js/withyou/data/dto/CategoryDto.java
@@ -1,15 +1,22 @@
 package com.js.withyou.data.dto;
 
+import com.js.withyou.data.dto.place.PlaceDto;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.stereotype.Service;
+import lombok.ToString;
+
+import java.util.List;
 
 @Getter
 @Setter
+@ToString
 public class CategoryDto {
     @NotBlank
     private Long categoryId;
     @NotBlank
     private String categoryName;
+
+    private List<PlaceDto> places;
+
 }
