b331427e42157f5b8f1545af6244972fcd048de1
JSH <skyjs135@gmail.com>
2024-03-22 23:04:57 +0900

    feat: 유저이메일(memberEmail)로 유저가 작성한 리뷰를 가져오는 메서드를 구현하였습니다.      * return값은 List<ReviewMypageDto>로 반환합니다.      * 해당 ReviewMypageDto는 리뷰의 id,내용,별점,작성시간,시설명을 담습니다.

--- src/main/java/com/js/withyou/service/ReviewService.java
+++ src/main/java/com/js/withyou/service/ReviewService.java
@@ -1,8 +1,11 @@
 package com.js.withyou.service;
 
 import com.js.withyou.data.dto.ReviewCreateDto;
+import com.js.withyou.data.dto.ReviewDto;
+import com.js.withyou.data.dto.place.ReviewMypageDto;
 import com.js.withyou.data.entity.Review;
 import com.js.withyou.repository.ReviewRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -11,4 +14,6 @@ public interface ReviewService {
     public List<Review> getReviewsByPlace(Long PlaceId);
 
     void createReview(ReviewCreateDto reviewCreateDto);
+
+    List<ReviewMypageDto> getReviewsByMemberEmail(@Param("memberEmail") String memberEmail);
 }
--- src/main/java/com/js/withyou/service/impl/ReviewServiceImpl.java
+++ src/main/java/com/js/withyou/service/impl/ReviewServiceImpl.java
@@ -2,7 +2,9 @@ package com.js.withyou.service.impl;
 
 import com.js.withyou.data.dto.MemberDto;
 import com.js.withyou.data.dto.ReviewCreateDto;
+import com.js.withyou.data.dto.ReviewDto;
 import com.js.withyou.data.dto.place.PlaceDto;
+import com.js.withyou.data.dto.place.ReviewMypageDto;
 import com.js.withyou.data.entity.Member;
 import com.js.withyou.data.entity.Place.Place;
 import com.js.withyou.data.entity.Review;
@@ -15,6 +17,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.ui.ModelMap;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @Slf4j
 public class ReviewServiceImpl implements ReviewService {
@@ -32,7 +36,6 @@ public class ReviewServiceImpl implements ReviewService {
 
     /**
      * 특정 장소(place)에 작성된 리뷰들을 가져옵니다.
-     *
      */
 
 
@@ -45,13 +48,13 @@ public class ReviewServiceImpl implements ReviewService {
 
     /**
      * 유저가 작성한 리뷰를
-//     * @param  reviewCreateDto reviewCreateDto 유저가 작성한 리뷰 정보를 담은 DTO
-     *
+     * //     * @param  reviewCreateDto reviewCreateDto 유저가 작성한 리뷰 정보를 담은 DTO
      */
     public void createReview(ReviewCreateDto reviewCreateDto) {
         Review review = new Review();
 
         Place foundPlace = placeService.getPlaceEntityByPlaceId(reviewCreateDto.getPlaceId());
+
         review.setPlace(foundPlace);
 
         Member foundMember = memberService.getMemberEntityByEmail(reviewCreateDto.getUserEmail());
@@ -61,8 +64,31 @@ public class ReviewServiceImpl implements ReviewService {
         Review savedReview = reviewRepository.save(review.saveReview(reviewCreateDto, foundMember, foundPlace));
 //        return savedReview;
 
-    };
+    }
 
+    ;
+
+
+    /**
+     *
+     * @param memberEmail
+     * @return
+     */
+    @Override
+    public List<ReviewMypageDto> getReviewsByMemberEmail(String memberEmail) {
+        List<Review> reviewByMemberEmail = reviewRepository.findReviewByMemberEmail(memberEmail);
+
+        List<ReviewMypageDto> reviewMypageDtos = reviewByMemberEmail.stream().map(review -> {
+            ReviewMypageDto reviewMypageDto = new ReviewMypageDto();
+            return reviewMypageDto.convertToReviewMypageDto(review);
+        }).collect(Collectors.toList());
+
+        for (ReviewMypageDto reviewMypageDto : reviewMypageDtos) {
+        }
+
+
+        return reviewMypageDtos;
+    }
 
 
     ;
