18391d6917542ae10db73e64ca9f6099678305b6
JSH <skyjs135@gmail.com>
2024-03-26 23:40:29 +0900

    docs: 게시글(post) 저장을 위한 DTO를 추가하였습니다.      * 제목,내용을 저장 합니다.

---
+++ src/main/java/com/js/withyou/data/dto/PostCreateDto.java
@@ -0,0 +1,15 @@
+package com.js.withyou.data.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class PostCreateDto {
+
+    private String postTitle;
+
+    private String postContent;
+}
