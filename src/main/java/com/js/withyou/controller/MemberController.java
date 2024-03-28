7dcd8f6337ae14e484565a73bcd0e09cc497f76b
JSH <skyjs135@gmail.com>
2024-03-28 23:49:07 +0900

    feat: spring security 적용하여 메서드 파라미터를 변경하였습니다.      * 기존: 서버에서 @SessionAttribute로 유저정보 전달받음      * 변경: spring security 방식으로 Authentication으로 유저 정보 전달받음

--- src/main/java/com/js/withyou/controller/MemberController.java
+++ src/main/java/com/js/withyou/controller/MemberController.java
@@ -1,17 +1,26 @@
 package com.js.withyou.controller;
 
+import com.js.withyou.customClass.CustomUser;
+import com.js.withyou.data.dto.MemberCreateDto;
 import com.js.withyou.data.dto.MemberDto;
 import com.js.withyou.data.dto.MemberNameDto;
 import com.js.withyou.data.dto.place.PlaceDto;
+import com.js.withyou.data.dto.review.ReviewMypageDto;
 import com.js.withyou.service.MemberLikePlaceService;
 import com.js.withyou.service.MemberService;
 import com.js.withyou.service.PlaceService;
+import com.js.withyou.service.ReviewService;
+import com.js.withyou.service.impl.UserDetailsServiceImpl;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -28,32 +37,39 @@ public class MemberController {
     private final MemberService memberService;
     private final MemberLikePlaceService memberLikePlaceService;
 
+    private final PasswordEncoder passwordEncoder;
+
+
+    private final ReviewService reviewService;
+
 //    private final PlaceService placeService;
 
     @Autowired
-    public MemberController(MemberService memberService, PlaceService placeService, MemberLikePlaceService memberLikePlaceService) {
+    public MemberController(MemberService memberService, PlaceService placeService, MemberLikePlaceService memberLikePlaceService, PasswordEncoder passwordEncoder, ReviewService reviewService) {
         this.memberService = memberService;
 //        this.placeService = placeService;
         this.memberLikePlaceService = memberLikePlaceService;
+        this.passwordEncoder = passwordEncoder;
+        this.reviewService = reviewService;
     }
 
-    @GetMapping(value = "")
-    public String showMemberHome() {
-
-        return "/member/memberHome";
-    }
+//    @GetMapping(value = "")
+//    public String showMemberHome() {
+//
+//        return "/member/memberHome";
+//    }
 
     @GetMapping(value = "/signup")
     public String showSignup(Model model) {
-        model.addAttribute("memberDto", new MemberDto());
+        model.addAttribute("memberCreateDto", new MemberCreateDto());
         return "/member/signup";
     }
 
     ;
 
     //회원가입
-    @PostMapping("")
-    public String addNewMember(@Validated @ModelAttribute("memberDto") MemberDto memberDto,
+    @PostMapping("/signup")
+    public String addNewMember(@Validated @ModelAttribute("memberDto") MemberCreateDto memberCreateDto,
                                BindingResult bindingResult,
                                Model model,
                                HttpServletRequest request) {
@@ -63,8 +79,8 @@ public class MemberController {
             model.addAttribute("errors", bindingResult.getAllErrors());
             return "/member/signup";
         } else {
-            MemberDto savedMember = memberService.saveMember(memberDto);
-            return "redirect:/members";
+            MemberDto savedMember = memberService.saveMember(memberCreateDto);
+            return "redirect:/mypage";
         }
 //        log.info("MemberPassWord={}", memberDto.getMemberPassword());
 //        log.info("MemberEmail={}", memberDto.getMemberEmail());
@@ -76,103 +92,127 @@ public class MemberController {
     //    로그인
     @GetMapping("/login")
     public String showLoginPage() {
-        return "/member/loginForm";
+//        return "/member/loginForm";
+        return "/member/login";
     }
 
 
-    @PostMapping("/login")
-    public String login(@RequestParam String memberEmail,
-                        @RequestParam String password,
-                        @RequestParam(required = false, defaultValue = "/") String redirectURL,
-                        HttpServletRequest request) {
-        log.info("redirectURL={}", redirectURL);
+    //Spring security 사용으로 더이상 사용하지 않는 메서드 입니다.
+//    @PostMapping("/login")
+//    public String login(@RequestParam String memberEmail,
+//                        @RequestParam String password,
+//                        @RequestParam(required = false, defaultValue = "/") String redirectURL,
+//                        HttpServletRequest request) {
+//        log.info("redirectURL={}", redirectURL);
+//
+//
+//        Optional<MemberDto> loginMember = memberService.login(memberEmail, password);
+//        if (loginMember.isEmpty() == true) {
+//            log.info("로그인 에러 검출");
+//            return "/member/loginForm";
+//        } else {
+//
+//        }
+//        //세션+쿠키(토큰) 방식으로 로그인 유저 저장
+//        //getSession(true) 세션이 없으면 만들고 있으면 반환
+//        //getSession(false) 세션이 없으면 새로만들지 않음
+//        HttpSession session = request.getSession(true);
+//        //세션에 로그인 회원 정보 보관
+//        session.setAttribute("LOGIN_MEMBER", loginMember.get().getMemberName());
+//        log.info("이메일정보확인", loginMember.get().getMemberEmail());
+//        session.setAttribute("LOGIN_MEMBER_EMAIL", loginMember.get().getMemberEmail());
+//
+//        log.info("Session LOGIN_MEMBER: {}", session.getAttribute("LOGIN_MEMBER"));
+//        log.info("Session LOGIN_MEMBER_EMAIL: {}", session.getAttribute("LOGIN_MEMBER_EMAIL"));
+//        return "redirect:/mypage";
+////        return "redirect:" + redirectURL;
+//
+//    }
 
-
-        Optional<MemberDto> loginMember = memberService.login(memberEmail, password);
-        if (loginMember.isEmpty() == true) {
-            log.info("로그인 에러 검출");
-            return "/member/loginForm";
-        } else {
-
-        }
-        //세션+쿠키(토큰) 방식으로 로그인 유저 저장
-        //getSession(true) 세션이 없으면 만들고 있으면 반환
-        //getSession(false) 세션이 없으면 새로만들지 않음
-        HttpSession session = request.getSession(true);
-        //세션에 로그인 회원 정보 보관
-        session.setAttribute("LOGIN_MEMBER", loginMember.get().getMemberName());
-        log.info("이메일정보확인", loginMember.get().getMemberEmail());
-        session.setAttribute("LOGIN_MEMBER_EMAIL", loginMember.get().getMemberEmail());
-
-        log.info("Session LOGIN_MEMBER: {}", session.getAttribute("LOGIN_MEMBER"));
-        log.info("Session LOGIN_MEMBER_EMAIL: {}", session.getAttribute("LOGIN_MEMBER_EMAIL"));
-        return "redirect:/mypage";
-//        return "redirect:" + redirectURL;
-
-    }
 
     ;
 
-    /**
-     * @param memberName
-     * @param memberEmail
-     * @param request
-     * @param model
-     * @return
-     * @ToDo 시설 즐겨찾기 기능
-     * @Todo 내가쓴 댓글 보기 기능
-     */
-    //(@SessionAttribute(name ="LOGIN_MEMBER",required = false session에 있는지 확인, 새로생성하진 않음
-    @GetMapping("/mypage")
-    public String mypage(@SessionAttribute(name = "LOGIN_MEMBER", required = false) String memberName,
-                         @SessionAttribute(name = "LOGIN_MEMBER_EMAIL", required = false) String memberEmail
-            , HttpServletRequest request,
-                         Model model) {
-
-//        HttpSession session = request.getSession(false);
-        if (memberName == null) {
-            log.info("로그인 하지 않은 사용자 login page로 redirect");
-            return "redirect:/login";
-        } else {
-
-            log.info("login member, mypage로 redirect");
-            log.info(String.valueOf(memberName));
-
-            //session 정보를 DTO에 담아서 전송합니다, DTO에는 유저 이름과 이메일만 담아서 전송합니다.
-            MemberNameDto memberNameDto = new MemberNameDto();
-            memberNameDto.setMemberName(memberName);
-            memberNameDto.setMemberEmail(memberEmail);
-
-            model.addAttribute("memberDto", memberNameDto);
-
-            return "/member/memberMyPage";
-        }
-
-
-//        memberId.equals(null) 값이 null일경우 equals호출할수 없음 Objects.equlas(memberId,null)이렇게 하자
-        /*
-        mypage 접속시 cookie 확인, 없거나 DB에서 조회가 안되면 로그인 페이지로 리다이렉트
-         */
-//        if ( Objects.equals(memberId, null)){
-//            log.info("로그인 하지 않은 사용자");
-//        return "redirect:/members/login";
-//       }else if(memberEmptyCheck(memberId)) {
-//            log.info("등록되지 않은 사용자 접속");
-//            return "redirect:/members/login";
-//        }else {
-//            model.addAttribute("memberId",memberId);
-//            return "/member/memberMyPage";
+//    @ResponseBody
+//    @GetMapping("/mypage")
+////    public String mypage(@SessionAttribute(name = "LOGIN_MEMBER", required = false) String memberName,
+////                         @SessionAttribute(name = "LOGIN_MEMBER_EMAIL", required = false) String memberEmail
+////            , HttpServletRequest request,
+////                         Model model) {
+//        log.info("로그인한 유저 닉네임 ={}",customUser.memberName);
+////        HttpSession session = request.getSession(false);
+//        if (authentication.isAuthenticated()) {
+//            log.info("로그인한 유저={}",authentication.getName());
+//            return "ok";
+//        } else {
+//            log.info("로그인 하지 않은 사용자 login page로 이동합니다.");
+//            return "redirect:/login";
+////            log.info("login member, mypage로 redirect");
+////            log.info(String.valueOf(memberName));
+//            //session 정보를 DTO에 담아서 전송합니다, DTO에는 유저 이름과 이메일만 담아서 전송합니다.
+////            MemberNameDto memberNameDto = new MemberNameDto();
+////            memberNameDto.setMemberName(memberName);
+////            memberNameDto.setMemberEmail(memberEmail);
+////            model.addAttribute("memberDto", memberNameDto);
+//
 //        }
 
-
+    @GetMapping("/mypage")
+    public String showMypage(Authentication authentication,
+                             Model model) {
+        //유저 정보를 가져옵니다.
+        CustomUser customUser = (CustomUser) authentication.getPrincipal();
+        log.info("로그인한 유저 닉네임 ={}", customUser.memberName);
+        if (authentication.isAuthenticated()) {
+            log.info("로그인한 유저={}", authentication.getName());
+            return "member/mypage";
+        } else {
+            log.info("로그인 하지 않은 사용자 login page로 이동합니다.");
+            return "redirect:/login";
+        }
     }
 
+
+
+
+    /**
+     * 유저가 본인이 좋아요 누른 시설을 보여주는 메서드 입니다.
+     *
+     * @param memberEmail
+     * @param model
+     * @return 좋아요 누른 시설(place)를 보여주는 view페이지를 반환합니다.
+     */
     @GetMapping("/mypage/likes/places")
     public String showMemberLikePlace(@SessionAttribute(name = "LOGIN_MEMBER_EMAIL", required = false) String memberEmail,
-                                       Model model) {
-        List<PlaceDto> placeDtoList = memberLikePlaceService.getPlaceDtosByMemberEmail(memberEmail);
+                                      Authentication authentication,
+                                      Model model) {
+
+        //유저 정보를 가져옵니다.
+        CustomUser customUser = (CustomUser) authentication.getPrincipal();
+
+        List<PlaceDto> placeDtoList = memberLikePlaceService.getPlaceDtosByMemberEmail(customUser.getUsername());
         model.addAttribute("placeDtoList", placeDtoList);
         return "/member/member-like-places";
+    }
+
+    @GetMapping("/mypage/reviews")
+    public String showReviewsByMember(@SessionAttribute(name = "LOGIN_MEMBER_EMAIL", required = false) String memberEmail,
+                                      Model model) {
+        //멤버가 작성한 리뷰들 찾아서 가져와주세요~
+        List<ReviewMypageDto> reviewsByMemberEmail = reviewService.getReviewsByMemberEmail(memberEmail);
+        model.addAttribute("reviews", reviewsByMemberEmail);
+        return "/member/member-reviews";
+    }
+
+    @ResponseBody
+    @GetMapping("/test52")
+    public String test52(Authentication authentication){
+//
+//        CustomUser customUser =  (CustomUser) SecurityContextHolder.getContext().getAuthentication();
+//        Object principal = customUser.toString();
+
+//        log.info("유저정보={}",principal.toString());
+        return "ok";
+
     }
 
     ;
--- src/main/java/com/js/withyou/controller/PlaceController.java
+++ src/main/java/com/js/withyou/controller/PlaceController.java
@@ -1,5 +1,6 @@
 package com.js.withyou.controller;
 
+import com.js.withyou.customClass.CustomUser;
 import com.js.withyou.data.dto.CategoryDto;
 import com.js.withyou.data.dto.place.PlaceDetailDto;
 import com.js.withyou.data.dto.place.PlaceDto;
@@ -7,8 +8,12 @@ import com.js.withyou.data.dto.Region.RegionDto;
 import com.js.withyou.service.CategoryService;
 import com.js.withyou.service.PlaceService;
 import com.js.withyou.service.RegionService;
+import com.js.withyou.service.MemberLikePlaceService;
 import jakarta.validation.constraints.NotNull;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -23,10 +28,13 @@ public class PlaceController {
     private final PlaceService placeService;
     private final RegionService regionService;
 
-    public PlaceController(CategoryService categoryService, PlaceService placeService, RegionService regionService) {
+    private final MemberLikePlaceService memberLikePlaceService;
+
+    public PlaceController(CategoryService categoryService, PlaceService placeService, RegionService regionService, MemberLikePlaceService memberLikePlaceService) {
         this.categoryService = categoryService;
         this.placeService = placeService;
         this.regionService = regionService;
+        this.memberLikePlaceService = memberLikePlaceService;
     }
 
     @GetMapping(value = "/places/new")
@@ -45,6 +53,7 @@ public class PlaceController {
 
     /**
      * 시설(place) 의 상세정보를 보여주는 메서드입니다.
+     *
      * @param placeId
      * @param model
      * @return
@@ -53,7 +62,11 @@ public class PlaceController {
     //    @ResponseBody
     @GetMapping("/places/{placeId}")
     public String showPlaceDetail(@PathVariable @NotNull Long placeId,
+                                  @SessionAttribute(value = "LOGIN_MEMBER_EMAIL", required = false) String memberEmail,
                                   Model model) {
+//        if (memberEmail.isEmpty()) {
+//            memberEmail = "kim@gmail.com";
+//        }
         log.info("요청된 placeId={}", placeId);
 //        id값 조회해서 해당 place 정보 view로 넘겨주세요
         PlaceDetailDto foundPlaceDto = placeService.getPlaceDetailDtoByPlaceId(placeId);
@@ -65,60 +78,155 @@ public class PlaceController {
         return "/place/place-detail";
     }
 
-    public void addPlaceDtoList(List<PlaceDto> placeDtoList, List<PlaceDto> foundPlaceDtoList){
+    public void addPlaceDtoList(List<PlaceDto> placeDtoList, List<PlaceDto> foundPlaceDtoList) {
         if (!foundPlaceDtoList.isEmpty()) {
             for (PlaceDto placeDto : foundPlaceDtoList) {
                 placeDtoList.add(placeDto);
             }
         }
-    };
+    }
+
+    ;
 
     /*
-    * 클라이언트가 검색어를 입력하면 그 검색어를 바탕으로 시설을 검색합니다.
-    * 1.키워드로 카테고리 검색 후 반환값이 null 이 아니면, 해당 카테고리 ID와 매핑된 시설을 저장합니다.
-    * 2.키워드로 시설명 검색 후 반환값이 null 이 아니면, 해당 시설들을 저장합니다.
-    * 3.키워드로 시도 정보 검색 후 반환값이 null 이 아니면, 해당 시도 하위 시군구 정보로 매핑된 시설을 저장합니다.
-    * 4.저장된 시설 정보를 클라이언트에게 반환합니다.
+     * 클라이언트가 검색어를 입력하면 그 검색어를 바탕으로 시설을 검색합니다.
+     * 1.키워드로 카테고리 검색 후 반환값이 null 이 아니면, 해당 카테고리 ID와 매핑된 시설을 저장합니다.
+     * 2.키워드로 시설명 검색 후 반환값이 null 이 아니면, 해당 시설들을 저장합니다.
+     * 3.키워드로 시도 정보 검색 후 반환값이 null 이 아니면, 해당 시도 하위 시군구 정보로 매핑된 시설을 저장합니다.
+     * 4.저장된 시설 정보를 클라이언트에게 반환합니다.
      */
     @GetMapping("/search")
     public String showSearchPlace(@RequestParam(name = "query", required = false) String searchKeyword
             , Model model) {
 
         log.info("searchKeyword={}", searchKeyword);
-        if (searchKeyword == null||searchKeyword.isEmpty()) {//쿼리파라미터 값이 null 이거나, 쿼리파라미터가 null이면  view로 이동
+        if (searchKeyword == null || searchKeyword.isEmpty()) {//쿼리파라미터 값이 null 이거나, 쿼리파라미터가 null이면  view로 이동
             log.info("검색어 미입력");
             return "/place/place-search";
         }
 
 
         List<PlaceDto> placeDtoList = placeService.searchPlacesByKeyWord(searchKeyword);
-        model.addAttribute("placeDtoList",placeDtoList);
+        model.addAttribute("placeDtoList", placeDtoList);
         return "/place/place-search";
-        }
+    }
 
-        //1.시도 정보로 검색 -> 풀네임, 줄임말 둘다임
+    //1.시도 정보로 검색 -> 풀네임, 줄임말 둘다임
 //        placeService.
-        //2. 시군구 정보로 검색
+    //2. 시군구 정보로 검색
 
     /**
-     *
      * @param model
      * @return
      */
 
     @GetMapping("/places/select")
-    public String showRegionSelectPage(Model model){
+    public String showRegionSelectPage(Model model) {
         List<RegionDto> regionDtoList = regionService.getAllRegions();
-        model.addAttribute("regionDtoList",regionDtoList);
+        model.addAttribute("regionDtoList", regionDtoList);
         return "/place/place-region-select";
     }
 
 
+    @ResponseBody
+    @PostMapping("/places/{placeId}/is_favorite")
+    public ResponseEntity<String> checkPlaceLike(@PathVariable(required = false) String placeId
+            , Authentication authentication) {
+
+//       시설(place) 정보 유무를 체크합니다.
+        if (placeId == null || !placeId.matches("^[0-9]+$")) {
+            log.info("좋아요 요청 에러 : placeId가 Null이거나 정수가 숫자가아닙니다.");
+            return new ResponseEntity<>("좋아요 요청 에러 : placeId가 Null이거나 정수가 숫자가아닙니다.", HttpStatus.BAD_REQUEST);
+        }
+        //user 로그인 정보를 확인합니다.
+        if (authentication == null) {
+            log.info("로그인 정보 없음");
+            return new ResponseEntity<>("false", HttpStatus.OK);//로그인 정보가 없으면 fasle (빈하트)
+        }
+        CustomUser customUser = (CustomUser) authentication.getPrincipal();
+        //테이블에서 좋아요를 눌렀는지 확인합니다. 있을겨우 true 없을경우 false
+        boolean checkReuslt = memberLikePlaceService.checkMemberLikePlaceByPlaceAndMemberExistence(Long.valueOf(placeId), customUser.getUsername());
+        if (checkReuslt) {
+            return new ResponseEntity<>("true", HttpStatus.OK);//좋아요 눌렀으면 true (속이찬 하트)
+        } else {
+            return new ResponseEntity<>("false", HttpStatus.OK);//좋아요 정보가 없으면 false (속이빈 하트)
+        }
     }
 
 
+    /**
+     * 유저가 시설(place)에 좋아요 버튼을 누르면 해당 정보를 저장하는 메서드입니다.
+     *
+     * @param placeId URL에 포함된 placeId를 문자열로 받습니다, 메서드에서 null체크와 숫자가 맞는지 체크합니다.
+     * @return 요청 결과를 반환합니다. 잘못된 요청일경우 요청메시지를 보냅니다.
+     */
+    @ResponseBody
+    @PostMapping("places/{placeId}/add_favorite")
+    public ResponseEntity<String> addPlaceFavorite(@PathVariable(required = false) String placeId
+            , @SessionAttribute(name = "LOGIN_MEMBER_EMAIL", required = false) String memberEmail,
+                                                   Authentication authentication) {
 
-    ;
+
+        if (placeId == null || !placeId.matches("^[0-9]+$")) {
+            log.info("좋아요 요청 에러 : placeId가 Null이거나 정수가 숫자가아닙니다.");
+            return new ResponseEntity<>("좋아요 요청 에러 : placeId가 Null이거나 정수가 숫자가아닙니다.", HttpStatus.BAD_REQUEST);
+        }
+        //user 로그인 정보를 확인합니다.
+        if (authentication == null) {
+            log.info("로그인 정보 없음");
+            return new ResponseEntity<>("false", HttpStatus.OK);
+        }
+        CustomUser customUser = (CustomUser) authentication.getPrincipal();
+        //좋아요 정보를 저장합니다. 성공했을경우 OK가 반환됩니다.
+        String state = memberLikePlaceService.saveMemberLikePlace(Long.valueOf(placeId), customUser.getUsername());
+        if (state == "OK") {
+            return new ResponseEntity<>("요청성공", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("요청 오류", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+
+    /**
+     * 유저가 시설(place)에 좋아요 버튼을 눌르면 해당 정보를 삭제하는 메서드입니다.
+     *
+     * @param placeId URL에 포함된 placeId를 문자열로 받습니다, 메서드에서 null체크와 숫자가 맞는지 체크합니다.
+     * @return 요청 결과를 반환합니다. 잘못된 요청일경우 요청메시지를 보냅니다.
+     */
+    @ResponseBody
+    @PostMapping("places/{placeId}/delete_favorite")
+    public ResponseEntity<String> removePlaceFavorite(@PathVariable(required = false) String placeId
+            , @SessionAttribute(name = "LOGIN_MEMBER_EMAIL", required = false) String memberEmail
+            , Authentication authentication
+    ) {
+
+        //URL의 placeId가 null이거나 숫자가 아닐경우 에러 메시지를 보냅니다.
+        if (placeId == null || !placeId.matches("^[0-9]+$")) {
+            log.info("좋아요 요청 에러 : placeId가 Null이거나 정수가 숫자가아닙니다.");
+            return new ResponseEntity<>("좋아요 요청 에러 : placeId가 Null이거나 정수가 숫자가아닙니다.", HttpStatus.BAD_REQUEST);
+        }
+
+        if (authentication == null) {
+            log.info("로그인 정보 없음");
+            return new ResponseEntity<>("false", HttpStatus.OK);
+        }
+        CustomUser customUser = (CustomUser) authentication.getPrincipal();
+        //placeId로 조회된 값이 없으면 에러메시지를 보냅니다.
+
+        String state = memberLikePlaceService.deleteMemberLikePlace(Long.valueOf(placeId), customUser.getUsername());
+        if (state == "OK") {
+            log.info("좋아요 삭제 요청 성공");
+            return new ResponseEntity<>("요청성공", HttpStatus.OK);
+        } else {
+            log.info("좋아요 삭제 요청 실패");
+            return new ResponseEntity<>("요청 오류", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
+
+
+;
 
 //    @GetMapping("/search")
 //    public String searchPlaces(@RequestParam(required = false) String searchWord,
