44a9c71bd85d15de542cd030b0af3b37470f65e4
JSH <skyjs135@gmail.com>
2024-03-31 23:55:12 +0900

    feat: 메서드를 수정, 추가하였습니다.      * createPost는 boolean 값을 return하여 성공 여부를 반환합니다.      * postList를 페이지네이션 기반으로 return하는 getPostList 메서드를 추가하였습니다.

--- src/main/java/com/js/withyou/service/PostService.java
+++ src/main/java/com/js/withyou/service/PostService.java
@@ -1,10 +1,23 @@
 package com.js.withyou.service;
 
 import com.js.withyou.data.dto.PostCreateDto;
+import com.js.withyou.data.dto.PostListDto;
 import com.js.withyou.data.dto.PostViewDto;
 
+import java.util.List;
+
 public interface PostService {
-    void createPost(PostCreateDto postCreateDto,String memberEmail);
+
+    /**
+     * post를 저장하는 메서드 입니다.
+     *
+     * @param postCreateDto
+     * @param memberEmail
+     * @return
+     */
+    boolean createPost(PostCreateDto postCreateDto, String memberEmail);
+
+    List<PostListDto> getPostList(int pageNumber, int pageSize);
 
     PostViewDto getPostById(Long postId);
 }
--- src/main/java/com/js/withyou/service/impl/PostServiceImpl.java
+++ src/main/java/com/js/withyou/service/impl/PostServiceImpl.java
@@ -1,38 +1,83 @@
 package com.js.withyou.service.impl;
 
 import com.js.withyou.data.dto.PostCreateDto;
+import com.js.withyou.data.dto.PostListDto;
 import com.js.withyou.data.dto.PostViewDto;
 import com.js.withyou.data.entity.Member;
 import com.js.withyou.data.entity.Post;
 import com.js.withyou.repository.PostRepository;
 import com.js.withyou.service.MemberService;
 import com.js.withyou.service.PostService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 
+@Slf4j
+
 public class PostServiceImpl implements PostService {
 
-  private final   PostRepository postRepository;
+    private final PostRepository postRepository;
     private final MemberService memberService;
+
     public PostServiceImpl(PostRepository postRepository, MemberService memberService) {
         this.postRepository = postRepository;
         this.memberService = memberService;
     }
 
+
+    @Transactional
     @Override
-    public void createPost(PostCreateDto postCreateDto,String memberEmail) {
-        Member member = memberService.getMemberEntityByEmail(memberEmail);
+    public boolean createPost(PostCreateDto postCreateDto, String memberEmail) {
+        try {
+            //멤버 entity 조회
+            Member member = memberService.getMemberEntityByEmail(memberEmail);
+            Post post = Post.builder()
+                    .postTitle(postCreateDto.getPostTitle())
+                    .postContent(postCreateDto.getPostContent())
+                    .member(member)
+                    .build();
+            postRepository.save(post);
+            return true;
+        } catch (Exception e){
+            log.info("post 저장 실패");
+            return false;
+        }
+    }
 
-        Post post = Post.builder()
-                .postTitle(postCreateDto.getPostTitle())
-                .postContent(postCreateDto.getPostContent())
-                .member(member)
-                .build();
+    @Override
+    public List<PostListDto> getPostList(Integer pageNumber) {
+        return null;
+    }
 
-        postRepository.save(post);
-
-
+    @Transactional(readOnly = true)
+    @Override
+    public List<PostListDto> getPostList(int pageNumber, int pageSize) {
+        PageRequest pageRequest = PageRequest.of(pageNumber,pageSize);
+        /**
+         * 전달받은 page 정보로 Post를 조회해서 반환합니다.
+         * 반환된 객체는 Page 타입임으로 안에 데이터를 꺼내야 합니다.
+         */
+        Page<Post> allPosts = postRepository.findAll(pageRequest);
+        //return용 객체입니다.
+        List<PostListDto> postListDtos = new ArrayList<>();
+        //repository에서 반환된 값을 PlstListDTo로 변경하여 반환합니다.
+       return postListDtos = allPosts.getContent()
+                .stream()
+                .map(Post -> {
+                    return PostListDto.builder()
+                            .postTitle(Post.getPostTitle())
+                            .postId(Post.getPostId())
+                            .memberName(Post.getMember().getMemberName())
+                            .build();
+                }).collect(Collectors.toList());
     }
 
     @Override
