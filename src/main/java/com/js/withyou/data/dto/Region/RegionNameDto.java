4fc637abf7230473930b5a5382f364c428c1da62
JSH <skyjs135@gmail.com>
2024-04-03 23:48:51 +0900

    docs: 시도(region) id와 시도이름만 전달하는 DTO를 추가하였습니다.      * 검색 힌트 등에서 사용하는 DTO입니다.

---
+++ src/main/java/com/js/withyou/data/dto/Region/RegionNameDto.java
@@ -0,0 +1,19 @@
+package com.js.withyou.data.dto.Region;
+
+import lombok.*;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class RegionNameDto {
+
+    private Long regionId;
+
+    private String regionName;
+
+
+
+
+}
