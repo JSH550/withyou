8a2b9d8c21f41e42bcdf170febdb2b6d722aee9e
JSH <skyjs135@gmail.com>
2024-04-08 23:31:15 +0900

    feat: 제약조건을 추가하였습니다.      * postTitle: NotBlank, size 50 제한      * postContent : NotBlank, size 2000 제한

--- src/main/java/com/js/withyou/data/dto/PostCreateDto.java
+++ src/main/java/com/js/withyou/data/dto/PostCreateDto.java
@@ -1,15 +1,23 @@
 package com.js.withyou.data.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.validator.constraints.Range;
 
 @Getter
 @Setter
 @ToString
 public class PostCreateDto {
 
+    @NotBlank
+    @Size(max = 50)
     private String postTitle;
 
+    @NotBlank
+    @Size(max = 2000)
     private String postContent;
 }
