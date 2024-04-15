4d80e40c8d0543495b0ace7f24ee9d5b43a8aba4
JSH <skyjs135@gmail.com>
2024-04-15 16:01:55 +0900

    feat: 유저(Member)의 닉네임을 변경한는 메서드를 추가하였습니다.      * 파라미터로 MemberNameDto를 받습니다.(email과 변경할 닉네임값 저장)      * DB에서 닉네임 조회 후 현재 닉네임과 새로운 닉네임이 같을때만 update 합니다.      * Transactional을 사용합니다.      * 변경에 성공하면 true를 반환합니다.

--- src/main/java/com/js/withyou/service/MemberService.java
+++ src/main/java/com/js/withyou/service/MemberService.java
@@ -1,12 +1,15 @@
 package com.js.withyou.service;
 
-import com.js.withyou.data.dto.MemberDto;
+import com.js.withyou.data.dto.member.MemberCreateDto;
+import com.js.withyou.data.dto.member.MemberDto;
+import com.js.withyou.data.dto.member.MemberNameDto;
+import com.js.withyou.data.dto.member.MemberPasswordDto;
 import com.js.withyou.data.entity.Member;
 
 import java.util.Optional;
 
 public interface MemberService {
-    MemberDto saveMember(MemberDto memberDto);
+    MemberDto saveMember(MemberCreateDto memberCreateDto);
 
     MemberDto getMemberDtoById(Long memberId);
 
@@ -15,4 +18,20 @@ public interface MemberService {
     Member getMemberEntityByEmail(String email);
 
     Optional<MemberDto> login(String memberName, String password);
+
+    /**
+     * 유저의 닉네임을 업데이트하는 메서드입니다.
+     * @param memberNameDto 유저의 email과 변경할 닉네임을 전달받습니다.
+     * @return
+     */
+    boolean updateMemberName(MemberNameDto memberNameDto);
+
+    /**
+     * 유저의 비밀번호를 업데이트하는 메서드입니다.
+     * @param memberPasswordDto 유저의 email과 변경할 password를 담고 있습니다.
+     * @return
+     */
+    boolean updateMemberPassword(MemberPasswordDto memberPasswordDto);
+
+
 }
--- src/main/java/com/js/withyou/service/impl/MemberServiceImpl.java
+++ src/main/java/com/js/withyou/service/impl/MemberServiceImpl.java
@@ -1,12 +1,19 @@
 package com.js.withyou.service.impl;
 
-import com.js.withyou.data.dto.MemberDto;
+import com.js.withyou.data.dto.member.MemberCreateDto;
+import com.js.withyou.data.dto.member.MemberDto;
+import com.js.withyou.data.dto.member.MemberNameDto;
+import com.js.withyou.data.dto.member.MemberPasswordDto;
 import com.js.withyou.data.entity.Member;
 import com.js.withyou.repository.MemberRepository;
 import com.js.withyou.service.MemberService;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.NoSuchElementException;
 import java.util.Optional;
@@ -15,21 +22,29 @@ import java.util.Optional;
 @Slf4j
 public class MemberServiceImpl implements MemberService {
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public MemberServiceImpl(MemberRepository memberRepository) {
+    public MemberServiceImpl(MemberRepository memberRepository, PasswordEncoder passwordEncoder) {
         this.memberRepository = memberRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
-    public MemberDto saveMember(MemberDto memberDto) {
+    public MemberDto saveMember(MemberCreateDto memberCreateDto) {
+        String encodedPassword = passwordEncoder.encode(memberCreateDto.getMemberPassword());//비밀번호를 인코딩합니다.
+//        Member member = Member.builder()
+//                .memberEmail(memberCreateDto.getMemberEmail())
+//                .memberPassword(encodedPassword)
+//                .memberName(memberCreateDto.getMemberName())
+//                .build();
+
         Member member = new Member();
         member.saveMember(
-                memberDto.getMemberEmail(),
-                memberDto.getMemberName(),
-                memberDto.getMemberPassword());
-//                memberDto.getMemberEmail(),
-//                memberDto.getMemberNickName());
+                memberCreateDto.getMemberEmail(),
+                memberCreateDto.getMemberName(),
+                encodedPassword);
+
         Member savedMember = memberRepository.save(member);
 
         return convertToDto(savedMember);
@@ -38,10 +53,10 @@ public class MemberServiceImpl implements MemberService {
     @Override
     public MemberDto getMemberDtoById(Long memberId) {
         Optional<Member> foundMember = memberRepository.findById(memberId);
-        if(foundMember.isPresent()){
+        if (foundMember.isPresent()) {
             Member foundMemberEntity = foundMember.get();
             return convertToDto(foundMemberEntity);
-        }else{
+        } else {
 
             throw new NoSuchElementException("Member not found with ID: " + memberId);
         }
@@ -51,31 +66,30 @@ public class MemberServiceImpl implements MemberService {
     public MemberDto getMemberDtoByName(String memberName) {
 
         Optional<Member> foundMemberByName = memberRepository.findByMemberName(memberName);
-        if(foundMemberByName.isPresent()){
+        if (foundMemberByName.isPresent()) {
             return convertToDto(foundMemberByName.get());
-        }else{
+        } else {
             throw new NoSuchElementException("Member not found with memberName: " + memberName);
         }
     }
 
     /**
      * Email로 유저 정보를 조회하는 메서드입니다.
+     *
      * @param email 조회할 계정의 Email 입니다.
      * @return Member entity 객체입니다. 컨트롤러로 전송하지 않게 주의합니다.(DTO를 반환하는 메서드를 이용해주세요)
      */
     @Override
     public Member getMemberEntityByEmail(String email) {
         Optional<Member> foundMember = memberRepository.findByMemberEmail(email);
-        if (foundMember.isPresent()){
+        if (foundMember.isPresent()) {
             return foundMember.get();
-        }else {
+        } else {
             throw new IllegalArgumentException("이메일 검색을 통한 유저 정보가 존재하지 않습니다.");
         }
     }
 
     ;
-
-
 
 
     @Override
@@ -92,8 +106,71 @@ public class MemberServiceImpl implements MemberService {
             ;
         }
         return Optional.empty();
-    };
+    }
 
+    @Transactional
+    @Override
+    public boolean updateMemberName(MemberNameDto memberNameDto) {
+
+        Optional<Member> foundMember = memberRepository.findByMemberEmail(memberNameDto.getMemberEmail());
+
+        boolean saveResult = false;//결과를 반환할 불리안 객체입니다. default값은 false입니다.
+        //요청한 유저의 email 검색 결과가 존재하고, 변경하고자 하는 닉네임이 현재의 닉네임과 다른지 확인합니다.
+        if (foundMember.isPresent() && !foundMember.get().getMemberName().equals(memberNameDto.getMemberName())) {
+            Member member = foundMember.get();
+            member.updateMemberName(memberNameDto.getMemberName());
+            Member updatedMember = memberRepository.save(member);
+            saveResult = updatedMember.getMemberEmail().equals(memberNameDto.getMemberEmail());//update 결과를 저장합니다.
+        }
+        return saveResult;
+
+    }
+
+    @Transactional
+    @Override
+    public boolean updateMemberPassword(MemberPasswordDto memberPasswordDto) {
+        boolean saveResult = false;//결과를 반환할 불리안 객체입니다. default값은 false입니다.
+
+            Optional<Member> foundMember = memberRepository.findByMemberEmail(memberPasswordDto.getMemberEmail());
+            if (foundMember.isEmpty()) {
+                log.info("패스워드 변경 요청 에러, 저장되지 않은 사용자");
+                throw new EntityNotFoundException("로그인 정보를 확인해 주세요");
+//            return saveResult = false;
+            }
+
+            String encodedNewPassword = passwordEncoder.encode(memberPasswordDto.getMemberPassword());//새로운 비밀번호를 인코딩합니다.
+            //새로운 비밀번호가 DB에 저장된 비밀번호와 같은지 확인합니다, 인코딩 되어있으므로 passwordEncoder.matches 메서드를 이용합니다.
+            if (passwordEncoder.matches(memberPasswordDto.getMemberPassword(), foundMember.get().getMemberPassword())) {
+                log.info("패스워드 변경 요청 에러, 기존 패스워드와 동일한 패스워드");
+                throw new DataIntegrityViolationException("패스워드 변경 요청 에러,기존과 동일한 패스워드 입니다. ");
+//            return saveResult = false;
+            }
+            ;
+//
+//        if (encodedNewPassword.equals(foundMember.get().getMemberPassword())) {
+//            log.info("패스워드 변경 요청 에러, 기존 패스워드와 동일한 패스워드");
+//            return saveResult = false;
+//        }
+
+            Member member = foundMember.get();//DB에서 찾아온 레코드 값을 객체에 저장합니다.
+            member.updateMemberPassword(encodedNewPassword);//비밀번호를 새로운 비밀번호로 설정합니다.
+            Member updatedMember = memberRepository.save(member);
+
+            //요청한 유저와 패스워드를 변경한 유저가 동일한지 확인합니다.
+            if (member.getMemberId() != updatedMember.getMemberId()) {
+                log.info("패스워드 변경 요청 에러, DB 저장 에러");
+
+                throw new DataIntegrityViolationException("패스워드 변경 요청 에러. 관리자에게 문의하세요 ");
+
+//                saveResult = true;
+            }
+
+            return saveResult = true;
+
+
+    }
+
+    ;
 
 
     private MemberDto convertToDto(Member member) {
