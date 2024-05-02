a37f6d5e9e66d2ddc1cf0aa38442551c2b830b61
JSH <skyjs135@gmail.com>
2024-05-02 22:09:05 +0900

    feat: 메서드를 추가하였습니다.      * updateMemberName : 유저가 닉네임 수정시에 사용될 메서드입니다.      * updateMemberPassword : 유저가 비밀번호 수정시에 사용될 메서드 입니다.

--- src/main/java/com/js/withyou/data/entity/Member.java
+++ src/main/java/com/js/withyou/data/entity/Member.java
@@ -11,8 +11,6 @@ import static jakarta.persistence.FetchType.LAZY;
 @Entity
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)//auto_increment 자동생성
@@ -23,6 +21,7 @@ public class Member {
     private String memberEmail;
 
     private String memberPassword;
+
     //이름
     private String memberName;
     
@@ -33,6 +32,25 @@ public class Member {
     //좋아요 누른 시설
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL,fetch = LAZY)
     private List<MemberLikePlace> memberLikePlace = new ArrayList<>();
+
+
+    /**
+     * 유저의 닉네임을 변경하기 위한 수정 메서드입니다.
+     * @param updateName
+     */
+    public void updateMemberName(String updateName){
+        this.memberName = updateName;
+    }
+
+
+    /**
+     * 유저의 닉네임을 변경하기 위한 수정 메서드입니다.
+     * @param password 인코딩된 password 입니다.
+     */
+    public void updateMemberPassword(String password){
+        this.memberPassword = password;
+    }
+
 
     
     //meber 저장 메서드
