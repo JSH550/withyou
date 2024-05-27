ba95ef6797955c72fe662da7b5060e2ee572a742
JSH <skyjs135@gmail.com>
2024-05-27 23:42:55 +0900

    docs: 진료과목(department)정보를 담을 엔티티,DTO를 추가하였습니다. * 중간테이블 PlaceDepartment에 1:N 매핑되어있습니다.

---
+++ src/main/java/com/js/withyou/data/dto/DepartmentNameDto.java
@@ -0,0 +1,19 @@
+package com.js.withyou.data.dto;
+
+import lombok.*;
+
+@Getter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class DepartmentNameDto {
+
+
+    private Integer departmentId;
+
+    private String departmentName;
+
+
+}
---
+++ src/main/java/com/js/withyou/data/entity/Department.java
@@ -0,0 +1,30 @@
+package com.js.withyou.data.entity;
+
+import com.js.withyou.data.PlaceDepartment;
+import com.js.withyou.data.entity.Place.Place;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@RequiredArgsConstructor
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer departmentId;
+
+    @Column(nullable = false,length = 30)
+
+    private String departmentName;
+
+
+    @OneToMany(mappedBy = "department",cascade =CascadeType.ALL, fetch =  FetchType.LAZY)
+    private List<PlaceDepartment> placeDepartments = new ArrayList<>();
+
+
+
+}
